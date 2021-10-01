62972e5c8ad283c0bf81e7690373abcd1b353685
juunini <juuni.ni.i@gmail.com>
2021-10-01 14:26:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DDD-Kurly-Clone-Front
---
+++ cypress/integration/app.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
+describe('Home', () => {
+  it('should render index page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/');
+
+    cy.contains('h1', 'Welcome to Next.js!');
+  })
+})
