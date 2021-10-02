d2c79eb0b5cdf62434f35e9a03a55bfef0023a6a
juunini <juuni.ni.i@gmail.com>
2021-10-02 21:41:59 +0900

    타이틀을 디폴트로 이동시킨다.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app';
 
+import Head from 'next/head';
+
 import 'styles/globals.css';
 
-function DefaultApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>마켓컬리 :: 내일의 장보기, 마켓컬리</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default DefaultApp;
+export default App;
