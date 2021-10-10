4891d808d8769c51e00a9f08065c3e52ca2a5a88
juunini <juuni.ni.i@gmail.com>
2021-10-10 14:57:50 +0900

    테스트에 의도를 드러내도록 수정합니다.

--- src/products/components/OriginPrice.test.jsx
+++ src/products/components/OriginPrice.test.jsx
@@ -4,9 +4,13 @@ import OriginPrice from 'src/products/components/OriginPrice';
 
 describe('OriginPrice component', () => {
   const givenOriginPrice = 8640;
+  const expectResult = '8,640'; // aka. givenOriginPrice.toLocaleString();
 
   it('should renders add comma what given number', () => {
     render(<OriginPrice>{givenOriginPrice}</OriginPrice>);
-    screen.getByText(givenOriginPrice.toLocaleString());
+
+    expect(
+      screen.getByText(expectResult),
+    ).toBeInTheDocument();
   });
 });
