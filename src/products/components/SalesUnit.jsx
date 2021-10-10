f3e942b7c8355a80b77474235b562b2834ee6f8f
juunini <juuni.ni.i@gmail.com>
2021-10-10 14:32:46 +0900

    판매단위 컴포넌트를 생성합니다.

---
+++ src/products/components/SalesUnit.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+/**
+ * @param { Object } Props Props.children is Product sales unit
+ * @param { React.ReactNode } Props.children Product sales unit
+ *
+ * @example
+ * <ProductSummary>
+ *   <SalesUnit>1pack</SalesUnit>
+ * </ProductSummary>
+ */
+export default function SalesUnit({ children }) {
+  return (
+    <>
+      <dt>판매단위</dt>
+      <dd>{children}</dd>
+    </>
+  );
+}
---
+++ src/products/components/SalesUnit.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+
+import SalesUnit from 'src/products/components/SalesUnit';
+
+describe('SalesUnit component', () => {
+  const givenSalesUnit = '1pack';
+
+  it('should renders given text', () => {
+    render(<SalesUnit>{givenSalesUnit}</SalesUnit>);
+    screen.getByText(givenSalesUnit);
+  });
+});
