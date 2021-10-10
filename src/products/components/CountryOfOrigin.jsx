b0e3722bcc24618bd2b3388217f6a9e48140e846
juunini <juuni.ni.i@gmail.com>
2021-10-10 14:45:23 +0900

    원산지 컴포넌트를 생성합니다.

---
+++ src/products/components/CountryOfOrigin.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+/**
+ * @param { Object } Props Props.children is Product country of origin
+ * @param { React.ReactNode } Props.children Product country of origin
+ *
+ * @example
+ * <ProductSummary>
+ *   <CountryOfOrigin>
+ *     Tomato(Korean)
+ *   </CountryOfOrigin>
+ * </ProductSummary>
+ */
+export default function CountryOfOrigin({ children }) {
+  return (
+    <>
+      <dt>원산지</dt>
+      <dd>{children}</dd>
+    </>
+  );
+}
---
+++ src/products/components/CountryOfOrigin.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+
+import CountryOfOrigin from 'src/products/components/CountryOfOrigin';
+
+describe('CountryOfOrigin component', () => {
+  const givenCountryOfOrigin = 'Tomato(Korean)';
+
+  it('should renders given text', () => {
+    render(<CountryOfOrigin>{givenCountryOfOrigin}</CountryOfOrigin>);
+    screen.getByText(new RegExp(givenCountryOfOrigin));
+  });
+});
