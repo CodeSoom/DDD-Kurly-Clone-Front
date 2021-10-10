1495f90f9ed46d9352c08dda9c95c61ac1cfe5c3
juunini <juuni.ni.i@gmail.com>
2021-10-10 22:50:58 +0900

    eslint disable 을 제거합니다.

--- src/products/components/ExpirationDate.jsx
+++ src/products/components/ExpirationDate.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable react/prop-types */
-
 /**
  * @param { Object } Props Props.children is Product expiration date
  * @param { React.ReactNode } Props.children Product expiration date
