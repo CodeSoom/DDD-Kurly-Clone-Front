6db2ee5fce0e2470a76550230e5276e6e457d8be
juunini <juuni.ni.i@gmail.com>
2021-10-10 15:28:00 +0900

    안내사항 컴포넌트를 생성합니다.

---
+++ src/products/components/Instructions.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+
+/**
+ * @param { Object } Props Props.children is Product instructions
+ * @param { React.ReactNode } Props.children Product instructions
+ *
+ * @example
+ * <ProductSummary>
+ *   <Instructions>
+ *     - Must Refrigerated.
+ *     - There may be a weight error of about 3%.
+ *   </Instructions>
+ * </ProductSummary>
+ */
+export default function Instructions({ children }) {
+  const instructionsValue = children
+    .split('\n')
+    .map((value) => value.trim())
+    .join('\n');
+
+  return (
+    <>
+      <dt>안내사항</dt>
+      <dd>
+        <pre>{instructionsValue}</pre>
+      </dd>
+    </>
+  );
+}
---
+++ src/products/components/Instructions.test.jsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+
+import Instructions from 'src/products/components/Instructions';
+
+describe('Instructions component', () => {
+  const givenInstructions = `- Must Refrigerated.
+- There may be a weight error of about 3%.`;
+
+  it('should renders given text', () => {
+    render(<Instructions>{givenInstructions}</Instructions>);
+
+    givenInstructions.split('\n').forEach((value) => {
+      expect(
+        screen.getByText(new RegExp(value, 'i')),
+      ).toBeInTheDocument();
+    });
+  });
+});
