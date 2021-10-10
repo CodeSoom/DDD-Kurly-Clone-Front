e7e2b07760f04b773e3fcb2c427cec689f86afaa
juunini <juuni.ni.i@gmail.com>
2021-10-10 14:36:28 +0900

    중량/용량 컴포넌트를 생성합니다.

---
+++ src/products/components/WeightCapacity.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+/**
+ * @param { Object } Props Props.children is Product weight or capacity
+ * @param { React.ReactNode } Props.children Product weight or capacity
+ *
+ * @example
+ * <ProductSummary>
+ *   <WeightCapacity>500g</WeightCapacity>
+ * </ProductSummary>
+ */
+export default function WeightCapacity({ children }) {
+  return (
+    <>
+      <dt>중량/용량</dt>
+      <dd>{children}</dd>
+    </>
+  );
+}
---
+++ src/products/components/WeightCapacity.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+
+import WeightCapacity from 'src/products/components/WeightCapacity';
+
+describe('WeightCapacity component', () => {
+  const givenWeightCapacity = '500g';
+
+  it('should renders given text', () => {
+    render(<WeightCapacity>{givenWeightCapacity}</WeightCapacity>);
+    screen.getByText(givenWeightCapacity);
+  });
+});
