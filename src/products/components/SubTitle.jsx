ec12c5b62642a9c1c34c491c7a526b87f5f5b696
juunini <juuni.ni.i@gmail.com>
2021-10-10 13:55:16 +0900

    상품 부제목 컴포넌트를 생성합니다.

---
+++ src/products/components/SubTitle.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+/**
+ * @param { Object } Props Props.children is Product Sub Title
+ * @param { React.ReactNode } Props.children Product Sub Title
+ *
+ * @example
+ * <SubTitle>
+ *   Very delicious cherry tomato without sugar
+ * </SubTitle>
+ */
+export default function SubTitle({ children }) {
+  return (
+    <p>
+      {children}
+    </p>
+  );
+}
---
+++ src/products/components/SubTitle.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+
+import SubTitle from 'src/products/components/SubTitle';
+
+describe('SubTitle component', () => {
+  const givenSubTitle = 'Very delicious cherry tomato without sugar';
+
+  it('should renders given text', () => {
+    render(<SubTitle>{givenSubTitle}</SubTitle>);
+    screen.getByText(givenSubTitle);
+  });
+});
