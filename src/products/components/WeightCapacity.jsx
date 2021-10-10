df6266558467baea9dbd07d09ad3943e0b209594
juunini <juuni.ni.i@gmail.com>
2021-10-10 22:53:25 +0900

    eslint-disable 을 제거합니다.

--- src/products/components/WeightCapacity.jsx
+++ src/products/components/WeightCapacity.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable react/prop-types */
-
 /**
  * @param { Object } Props Props.children is Product weight or capacity
  * @param { React.ReactNode } Props.children Product weight or capacity
