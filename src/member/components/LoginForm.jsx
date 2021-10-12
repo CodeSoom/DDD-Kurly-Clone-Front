c45f8812f594852e54dd0e8db51827263f33bcc4
daadaadaah <daadaadaah@gmail.com>
2021-10-13 05:05:32 +0900

    LoginForm 컴포넌트의 이벤트 핸들러들을 부모에서 주입해주는 방식으로 리팩터링

--- src/member/components/LoginForm.jsx
+++ src/member/components/LoginForm.jsx
@@ -1,17 +1,17 @@
 import Link from 'next/link';
 
-export default function LoginForm() {
-  const loginForm = {
-    id: 'codesoomer',
-    password: 'codesoomer-password',
-  };
+export default function LoginForm({
+  loginForm,
+  onChange,
+  onSubmit,
+}) {
+  const handleChangeField = (event) => {
+    const { name, value } = event.target;
 
-  const handleChangeField = () => {
-    // TODO : slice 구현 후 필요한 로직 추가 예정
-  };
-
-  const handleSubmitLoginForm = () => {
-    // TODO : slice 구현 후 필요한 로직 추가  및 메인페이지 완성 후 메인 페이지로 넘어가게 구현하기
+    onChange({
+      name,
+      value,
+    });
   };
 
   return (
@@ -27,6 +27,7 @@ export default function LoginForm() {
         placeholder="아이디를 입력해주세요"
       />
       <input
+        data-testid="login-password"
         id="input-password"
         type="password"
         name="password"
@@ -35,7 +36,7 @@ export default function LoginForm() {
         aria-label="password"
         placeholder="비밀번호를 입력해주세요"
       />
-      <button type="button" onClick={handleSubmitLoginForm}>로그인</button>
+      <button type="button" onClick={onSubmit}>로그인</button>
       <Link href="/signup">회원가입</Link>
     </fieldset>
   );
--- src/member/components/LoginForm.test.jsx
+++ src/member/components/LoginForm.test.jsx
@@ -1,12 +1,60 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
+  const loginForm = {
+    id: 'codesoomer',
+    password: 'codesoomer-password',
+  };
+
+  const handleChangeField = jest.fn();
+  const handleSubmitForm = jest.fn();
+
+  function renderLoginForm() {
+    return render(
+      <LoginForm
+        loginForm={loginForm}
+        onChange={handleChangeField}
+        onSubmit={handleSubmitForm}
+      />,
+    );
+  }
+
   it('renders LoginForm', () => {
-    render(<LoginForm />);
+    renderLoginForm();
 
     expect(screen.getAllByText('로그인')[0]).toBeInTheDocument();
     expect(screen.getByText('회원가입')).toBeInTheDocument();
+  });
+
+  it('listens the change event', () => {
+    renderLoginForm();
+
+    fireEvent.change(screen.getByTestId('login-id'), {
+      target: {
+        name: 'id',
+        value: 'newCodesoomer',
+      },
+    });
+
+    fireEvent.change(screen.getByTestId('login-password'), {
+      target: {
+        name: 'password',
+        value: 'codesoomer-newPassword',
+      },
+    });
+
+    expect(handleChangeField).toBeCalledTimes(2);
+  });
+
+  it('listens the click event', () => {
+    renderLoginForm();
+
+    fireEvent.click(screen.getByRole('button', {
+      name: '로그인',
+    }));
+
+    expect(handleSubmitForm).toBeCalledTimes(1);
   });
 });
