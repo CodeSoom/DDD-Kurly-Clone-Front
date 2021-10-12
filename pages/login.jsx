ec41b5c7bc7b687dec4e376edad6e8198fefa716
daadaadaah <daadaadaah@gmail.com>
2021-10-13 05:30:19 +0900

    <LoginPage />에서 <LoginForm/>의  이벤트핸들러 관리하도록 리팩터링

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,10 +1,27 @@
 import LoginForm from '../src/member/components/LoginForm';
 
 export default function LoginPage() {
+  const loginForm = {
+    id: 'codesoomer',
+    password: 'codesoomer-password',
+  };
+
+  const handleChangeField = () => {
+    // TODO : slice 구현 후 필요한 로직 추가 예정
+  };
+
+  const handleSubmitForm = () => {
+    // TODO : slice 구현 후 필요한 로직 추가  및 메인페이지 완성 후 메인 페이지로 넘어가게 구현하기
+  };
+
   return (
     <>
       <h2>로그인</h2>
-      <LoginForm />
+      <LoginForm
+        loginForm={loginForm}
+        onChange={handleChangeField}
+        onSubmit={handleSubmitForm}
+      />
     </>
   );
 }
