2eb25a6e59a50c3d6907b27417f765bf903a860b
dngwoodo <dngwoodo@gmail.com>
2021-10-12 14:10:57 +0900

    feat: SignUpForm 마크업 구현

---
+++ pages/signup.jsx
@@ -0,0 +1,10 @@
+import SignUpFormContainer from '../src/member/containers/SignUpFormContainer';
+
+export default function SignUp() {
+  return (
+    <section>
+      <h2>회원가입</h2>
+      <SignUpFormContainer />
+    </section>
+  );
+}
