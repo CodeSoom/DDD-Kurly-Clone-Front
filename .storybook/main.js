4c647debe164f596a1efd4500eaab9130379a6d0
juunini <juuni.ni.i@gmail.com>
2021-10-18 21:21:58 +0900

    fix: 스토리북의 절대경로를 설정합니다.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,22 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials'
   ],
-  "core": {
-    "builder": "webpack5"
-  }
+  core: {
+    'builder': 'webpack5'
+  },
+  typescript: { reactDocgen: false },
+  webpackFinal: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      src: path.resolve(__dirname, '../src'),
+    };
+  
+    return config;
+  },
 }
