ad2b8de9fae039993689772d785be7079c04f86f
dngwoodo <dngwoodo@gmail.com>
2021-10-21 21:06:19 +0900

    env: 유저 스키마 셋팅

---
+++ pages/api/member/index.type.ts
@@ -0,0 +1,12 @@
+export type ID = string;
+export type Name = string;
+export type Contact = {
+  address: string;
+  phone: string
+};
+
+export type Order = string;
+export type Orders = Order[];
+
+export type Coupon = string;
+export type Coupons = Coupon[];
